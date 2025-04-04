2c5c9ace3a41ac3a418f818b08d7d3a5d75bed11
mentai-mayo <102917864+mentai-mayo@users.noreply.github.com>
2025-04-04 19:26:06 +0900

    wip

--- main.go
+++ main.go
@@ -1,7 +1,93 @@
 package cli
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+)
 
 func Parse[T any](args []string) (*T, error) {
-	return nil, errors.New("Not implemented")
+	rt := reflect.TypeOf((*T)(nil)).Elem()
+	rv := reflect.New(rt).Elem()
+
+	// check is T struct
+	if rv.Kind().String() != "struct" {
+		return nil, NewNonStructTargetErr(rv.Kind().String())
+	}
+
+	// get expect names/types
+	expects := make([]Expect, 0, rt.NumField())
+	for i := 0; i < rt.NumField(); i++ {
+		rf := rt.Field(i)
+
+		// check is private
+		if rf.PkgPath == "" {
+			continue
+		}
+
+		// check expected type
+		switch rf.Type.String() {
+		case "string", "int", "uint", "bool":
+		default:
+			return nil, errors.New("unsupported expected type detected")
+		}
+		etype := rf.Type.String()
+
+		// check position
+		tag, ok := rf.Tag.Lookup("pos")
+		var position int
+		if ok {
+			num, err := strconv.Atoi(tag)
+			if err != nil {
+				return nil, errors.New("tag(\"pos\") must be parseable as a integer")
+			}
+			position = num
+		} else {
+			position = -1
+		}
+
+		// check long option name
+		tag, ok = rf.Tag.Lookup("long")
+		var long string
+		if ok {
+			long = tag
+		} else {
+			long = strings.ToLower(rf.Name)
+		}
+
+		// check short option name
+		tag, ok = rf.Tag.Lookup("short")
+		var short string
+		if ok {
+			short = tag
+		} else {
+			short = ""
+		}
+
+		// add
+		expects = append(expects, Expect{position, long, short, etype})
+	}
+
+	return nil, errors.New("Unimplemented")
+}
+
+type Expect struct {
+	position int
+	long     string
+	short    string
+	etype    string // string, int, uint, bool
+}
+
+type NonStructTargetErr struct {
+	actual string
+}
+
+func NewNonStructTargetErr(actual string) NonStructTargetErr {
+	return NonStructTargetErr{actual}
+}
+
+func (e NonStructTargetErr) Error() string {
+	return fmt.Sprintf("Non-Struct Target Error: arguments cannot be parsed for non-struct types (got: %s)", e.actual)
 }
--- main_test.go
+++ main_test.go
@@ -4,16 +4,35 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/mentai-mayo/cli-go"
 )
 
 type Arguments struct {
-	name string `arg:"1"`
+	name string `pos:"1"`
+}
+
+type ErrArguments struct {
+	name Arguments
 }
 
 func TestParse(t *testing.T) {
 	rawargs := []string{"bin", "mentai-mayo"}
 	_, err := cli.Parse[Arguments](rawargs)
-	assert.NoError(t, err)
+	require.NoError(t, err)
+}
+
+func TestParse_ErrNonStructTarget(t *testing.T) {
+	rawargs := []string{"bin", "mentai-mayo"}
+	_, err := cli.Parse[string](rawargs)
+	_, ok := err.(cli.NonStructTargetErr)
+	assert.True(t, ok, err)
+}
+
+func TestParse_ErrInvliadExpectedType(t *testing.T) {
+	rawargs := []string{"bin", "mentai-mayo"}
+	_, err := cli.Parse[ErrArguments](rawargs)
+	_, ok := err.(*cli.NonStructTargetErr)
+	assert.True(t, ok, err)
 }
